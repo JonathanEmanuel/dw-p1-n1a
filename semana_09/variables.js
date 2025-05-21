502f9181d469a9aac5c2325d3da5edd3ff277d2a
jcruzemae <107081665+jcruzemae@users.noreply.github.com>
2025-05-21 18:59:40 -0300

    Semana 09 - Repaso de Variables y Operadores

---
+++ semana_09/variables.js
@@ -0,0 +1,9 @@
+let nombre = "Jonathan"; // String
+let edad = 37; // Number
+let esProgramador = true; // Boolean
+const dni = 12345;
+
+
+nombre = prompt("Ingrese el nombre");
+
+console.log(nombre, edad, esProgramador);
